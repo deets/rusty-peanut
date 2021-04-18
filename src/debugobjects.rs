0b0432c010d8068c8b419ecc180fd17e5e4efbd6
Diez B. Roggisch <deets@web.de>
2021-04-18 19:57:31 +0200

    Introducing multiple signals and began proper parametrization

--- src/debugobjects.rs
+++ src/debugobjects.rs
@@ -1,19 +1,38 @@
+use nannou::prelude::*;
 use std::collections::hash_map::HashMap;
 use std::vec::Vec;
 use std::collections::VecDeque;
 use log::{debug, warn};
 
+type Rect = nannou::geom::rect::Rect;
+type Color = Rgb<u8>;
+
 pub trait DebugProcessor
 {
     fn name(&self) -> String;
+    fn draw(&self, draw: &nannou::draw::Draw);
+}
+
+struct ScopeSignal
+{
+    name: String,
+    min: f32,
+    max: f32,
+    y_size: Option<f32>,
+    y_base: Option<f32>,
+    //{legend
+    color: Color,
+    pub values: VecDeque<f32>,
 }
 
 pub struct Scope
 {
-    name: String,
-    pub values: VecDeque<f32>,
-    length: usize
 
+    name: String,
+    samples: usize, // Number of retained samples
+    rect: Rect,
+
+    signals: Vec<ScopeSignal>
 }
 
 impl Scope {
@@ -21,24 +40,85 @@ impl Scope {
     pub fn new(tokens: &Vec<String>) -> Scope
     {
 	assert!(tokens.len() >= 1);
+
 	let mut values = VecDeque::new();
 	values.push_back(0.0);
 	values.push_back(0.0);
-	Scope{ name: tokens[0].clone(), values, length: 128 }
+
+	let size: usize = 256;
+	let height = 256.0;
+	let width = 255.0;
+
+	let mut res = Scope{
+	    name: tokens[0].clone(),
+	    samples: size,
+	    rect: Rect::from_x_y_w_h(0.0, 0.0, width, height),
+	    signals: vec![],
+	};
+	res.signals.push(
+	    ScopeSignal
+	    {
+		name: "Test".to_string(),
+		min: -100.0,
+		max: 100.0,
+		y_size: None,
+		y_base: None,
+		color: GREEN,
+		values: VecDeque::from(vec![0.0, 0.0])
+	    }
+	);
+	res
     }
 
-    pub fn feed(&mut self, value: f32)
+    pub fn feed(&mut self, values: Vec<f32>)
     {
-	self.values.push_back(value);
-	while self.values.len() >= self.length {
-	    self.values.pop_front();
+	if values.len() != self.signals.len() {
+	    warn!("Scope<{}>::feed values and signals length differ", self.name);
 	}
+	let samples = self.samples;
+	self.signals.iter_mut().zip(values)
+	    .for_each(|(signal, value)| {
+		signal.values.push_back(value.clamp(signal.min, signal.max));
+		while signal.values.len() >= samples {
+		    signal.values.pop_front();
+		}
+	    });
     }
 }
 
 impl DebugProcessor for Scope {
     fn name(&self) -> String {
 	self.name.clone()
+    }
+
+    fn draw(&self, draw: &nannou::draw::Draw)
+    {
+	let xy = self.rect.xy();
+	let wh = self.rect.wh();
+	let step = wh.x / (self.samples as f32 - 1.0);
+	draw.rect().xy(xy).wh(wh).color(YELLOW);
+	self.signals.iter().for_each(|signal| {
+	    // Create an iterator yielding triangles for drawing a sine wave.
+	    let tris = signal.values.iter().zip(signal.values.iter().skip(1)).enumerate()
+		.flat_map(|(i, (left, right))| {
+		    let l_x = step * i as f32;
+		    let r_x = step * (i + 1) as f32;
+		    let a = pt2(l_x, map_range(*left, signal.min, signal.max, 0.0, wh.y));
+		    let b = pt2(r_x, map_range(*right, signal.min, signal.max, 0.0, wh.y));
+		    let c = pt2(r_x, map_range(0.0, signal.min, signal.max, 0.0, wh.y));
+		    let d = pt2(l_x, map_range(0.0, signal.min, signal.max, 0.0, wh.y));
+		    geom::Quad([a, b, c, d]).triangles_iter()
+		})
+		.map(|tri| {
+		    // Color the vertices based on their amplitude.
+		    tri.map_vertices(|v| {
+			let y_fract = map_range(v.y.abs(), 0.0, wh.y, 0.0, 1.0);
+			(v, signal.color)
+		    })
+		});
+	    // Draw the mesh!
+	    draw.xy(-wh / 2.0).mesh().tris_colored(tris);
+	});
     }
 }
 
@@ -52,6 +132,13 @@ impl DebugProcessor for DebugObject
     fn name(&self) -> std::string::String {
 	match self {
 	    DebugObject::Scope(scope) => scope.name()
+	}
+    }
+
+    fn draw(&self, draw: &nannou::draw::Draw)
+    {
+	match self {
+	    DebugObject::Scope(scope) => { scope.draw(draw); }
 	}
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![feature(clamp)]
 use nannou::prelude::*;
 
 mod serial;
@@ -22,42 +23,15 @@ fn update(app: &App, model: &mut Model, _update: Update)
 {
     let t = app.time;
     let value = (t * HZ * TAU).sin() * AMP;
-    model.scope.feed(value);
+    model.scope.feed(vec![value]);
 }
 
 
 fn view(app: &App, model: &Model, frame: Frame) {
     // Begin drawing
-    let win = app.window_rect();
-    let t = app.time;
     let draw = app.draw();
-
-    // Clear the background to black.
     draw.background().color(BLACK);
-
-    // Create an iterator yielding triangles for drawing a sine wave.
-    let tris = model.scope.values.iter().zip(model.scope.values.iter().skip(1)).enumerate()
-	.flat_map(|(i, (left, right))| {
-	    let l_x = (i * 5) as f32 / 10.0;
-	    let r_x = ((i + 1) * 5) as f32 / 10.0;
-            let a = pt2(l_x, *left);
-            let b = pt2(r_x, *right);
-            let c = pt2(r_x, 0.0);
-            let d = pt2(l_x, 0.0);
-            geom::Quad([a, b, c, d]).triangles_iter()
-	})
-        .map(|tri| {
-            // Color the vertices based on their amplitude.
-            tri.map_vertices(|v| {
-                let y_fract = map_range(v.y.abs(), 0.0, win.top(), 0.0, 1.0);
-                let color = srgba(y_fract, 1.0 - y_fract, 1.0 - y_fract, 1.0);
-                (v, color)
-            })
-        });
-
-    // Draw the mesh!
-    draw.mesh().tris_colored(tris);
-
+    model.scope.draw(&draw);
     // Write the result of our drawing to the window's frame.
     draw.to_frame(app, &frame).unwrap();
 }
