f4c70a4faab4065298af0d68990df8aedb2dca8e
Diez B. Roggisch <deets@web.de>
2021-04-18 12:37:34 +0200

    Began implementing rusty peanut

---
+++ src/debugobjects.rs
@@ -0,0 +1,131 @@
+use std::collections::hash_map::HashMap;
+use std::vec::Vec;
+use log::{debug, warn};
+
+trait DebugProcessor
+{
+    fn name(&self) -> String;
+}
+
+pub struct Scope
+{
+    name: String
+}
+
+impl Scope {
+
+    fn new(tokens: &Vec<String>) -> Scope
+    {
+	assert!(tokens.len() >= 1);
+	Scope{ name: tokens[0].clone() }
+    }
+}
+
+impl DebugProcessor for Scope {
+    fn name(&self) -> String {
+	self.name.clone()
+    }
+}
+
+pub enum DebugObject
+{
+    Scope(Scope)
+}
+
+impl DebugProcessor for DebugObject
+{
+    fn name(&self) -> std::string::String {
+	match self {
+	    DebugObject::Scope(scope) => scope.name()
+	}
+    }
+}
+
+pub struct DebugObjects
+{
+    objects: HashMap<String, DebugObject>
+}
+
+impl DebugObjects
+{
+    pub fn new() -> DebugObjects
+    {
+	DebugObjects{objects: HashMap::new()}
+    }
+}
+
+struct DebugLine
+{
+    keyword: String,
+    tokens: Vec<String>
+}
+
+fn parse_line(line: &str) -> std::result::Result<DebugLine, String>
+{
+    let tokens:Vec<String> = line.split_whitespace().map(|s| { s.to_string() }).filter(|part| { part.len() > 0 }).collect();
+    if tokens.len() > 0{
+	let mut keyword = tokens[0].clone();
+	if keyword.starts_with("`") {
+	    keyword = keyword[1..].to_string();
+	    return Ok(DebugLine{keyword: keyword, tokens: tokens[1..].to_vec()});
+	}
+    }
+    Err(format!("Can't parse line '{}'", line))
+}
+
+impl DebugObjects
+{
+    pub fn feed(&mut self, line: &str)
+    {
+	debug!("feeding line {}", line);
+	let line = parse_line(line).expect("error parsing line");
+	if self.objects.contains_key(&line.keyword) {
+	    debug!("found DebugObject, feeding to it");
+	} else {
+	    debug!("no DebugObject for keyword  {} - trying to create one", line.keyword);
+	    match self.create(&line.keyword, &line.tokens)
+	    {
+		Some(new_object) => {
+		    self.objects.insert(new_object.name(), new_object);
+		},
+		_ => { warn!("No factory found for {}", line.keyword); }
+	    }
+	}
+    }
+
+    fn create(&self, keyword: &str, tokens: &Vec<String>) -> Option<DebugObject>
+    {
+	// We need at least one additional token afetr the
+	// name, which will become the identifier.
+	if tokens.len() >= 1 {
+	    if keyword == "SCOPE" {
+		debug!("Createda Scope object named {}", tokens[0]);
+		return Some(DebugObject::Scope(Scope::new(tokens)))
+	    }
+	}
+	None
+    }
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test_env_log::test;
+
+      const SCOPE_DECLARATION:&str = "`SCOPE MyScope SIZE 254 84 SAMPLES 128\r\n\
+`MyScope 'Sawtooth' 0 63 64 10 %1111\r\n\
+`MyScope 31\r\n\
+`MyScope 32\r\n\
+`MyScope 33\r\n\
+`MyScope 34\r\n\
+`MyScope 35\r\n\
+`MyScope 36\r\n\
+";
+
+    #[test]
+    fn instantiate_scope_through_debug_objects() {
+	let mut debug_objects = DebugObjects::new();
+	debug_objects.feed("`SCOPE MyScope SIZE 254 84 SAMPLES 128");
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use core::time::Duration;
+
+mod debugobjects;
+
+const BAUD:u32 = 230_400;
+const PORT:&str = "/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_00000000-if00-port0";
+
+fn work() -> serialport::Result<()>
+{
+    let mut port = serialport::new(PORT, BAUD).open()?;
+    port.set_timeout(Duration::from_millis(1000))?;
+    loop {
+	let mut buffer: [u8; 1024] = [0; 1024];
+	match port.read(&mut buffer)
+	{
+	    Ok(bytes_read) => {
+		if let Ok(s) = std::str::from_utf8(&buffer[0..bytes_read])
+		{
+		    print!("{}", s);
+		}
+	    }
+	    Err(error) => {
+		println!("error: {:?}", error);
+	    }
+	}
+    }
+}
+
+fn main() {
+    println!("Hello, world! This is Rusty Peanut!");
+    work().expect("Work failed!");
+}
